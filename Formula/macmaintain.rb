06c85aad713526503d0cf226b35d9b09e6dbe613
Danh Nhan Phung <nhan.phung@itgrate.com>
2026-04-01 20:34:37 +0700

    chore: set bundled formula version to v1.0.5

--- Formula/macmaintain.rb
+++ Formula/macmaintain.rb
@@ -3,6 +3,7 @@ class Macmaintain < Formula
   homepage "https://github.com/pdnhan/mac-maintenance-script"
   url "https://github.com/pdnhan/mac-maintenance-script/archive/refs/tags/v1.0.5.tar.gz"
   sha256 "f6ca4a3740f1369999e243148c5a689d3fd336dcfa19fa7d9db73224d8e8140b"
+  version "1.0.5"
   license "MIT"
 
   def install
