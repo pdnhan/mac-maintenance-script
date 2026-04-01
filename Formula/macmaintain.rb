55fb12747d565f29de37fff23d01953729c6a280
Danh Nhan Phung <nhan.phung@itgrate.com>
2026-04-01 17:07:27 +0700

    fix: update formula SHA256 for v1.0.0

--- Formula/macmaintain.rb
+++ Formula/macmaintain.rb
@@ -2,7 +2,7 @@ class Macmaintain < Formula
   desc "Mac maintenance and optimization checker"
   homepage "https://github.com/pdnhan/mac-maintenance-script"
   url "https://github.com/pdnhan/mac-maintenance-script/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "PLACEHOLDER_REPLACE_WITH_ACTUAL_SHA256"
+  sha256 "e4b479b989e569ef681d6b00d6750810565e41b231ab099890007dc22ffdeeb4"
   license "MIT"
 
   depends_on "bash"
