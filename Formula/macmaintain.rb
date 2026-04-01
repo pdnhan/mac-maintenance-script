49a6ebf7292ce195d576019144e698b5ec141901
Danh Nhan Phung <nhan.phung@itgrate.com>
2026-04-01 18:01:17 +0700

    chore: update bundled formula to v1.0.4

--- Formula/macmaintain.rb
+++ Formula/macmaintain.rb
@@ -1,8 +1,8 @@
 class Macmaintain < Formula
   desc "Mac maintenance and optimization checker"
   homepage "https://github.com/pdnhan/mac-maintenance-script"
-  url "https://github.com/pdnhan/mac-maintenance-script/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "e013f61b88c0ebb79be73430a4fd76750ec66e65bd7acd5009390b21920dbe04"
+  url "https://github.com/pdnhan/mac-maintenance-script/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "bda2de2c91578733e70aec2c21dad06253a0c922e31e878da70520b225a64503"
   license "MIT"
 
   def install
